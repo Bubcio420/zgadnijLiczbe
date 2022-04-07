b18e6a1f0b59ba95bdd36d7babf132da6ce9acf9
Bubcio420 <97967665+Bubcio420@users.noreply.github.com>
2022-04-07 12:57:05 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-var wylosowanaliczba=Math.floor(Math.random()*100)+1;
+//losowanie liczby
+var wylosowanaLiczba=Math.floor(Math.random()*100)+1;
+function wyborLiczby(){
+    var ilosc=document.getElementById("zakres").value;
+    document.getElementById("wybranaliczba").value=ilosc;
+    wylosowanaLiczba=Math.floor(Math.random()*ilosc)+1;
+    iloscProb=0;
+}
+//liczba prób
 var iloscProb=0
 function sprawdzLiczbe(){
     iloscProb++;
     dane=document.getElementById('liczba')
-    if(dane.value==wylosowanaliczba){
+    if(dane.value == wylosowanaLiczba){
         alert("Wygrałeś. Ilość prób to "+iloscProb)
-    }else if(dane.value<wylosowanaliczba){
+    }else if(dane.value<wylosowanaLiczba){
         alert("Za mała liczba")
     }else{
         alert("Za duża liczba")
